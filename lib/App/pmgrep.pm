1688732c2fe4724de172384761bdf9589120e3fd
toritori0318 <toritori0318@gmail.com>
2011-02-19 23:25:43 +0900

    first commit

---
+++ lib/App/pmgrep.pm
@@ -0,0 +1,31 @@
+package App::pmgrep;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+App::pmgrep - Grep module
+
+=head1 DESCRIPTION
+
+App::pmgrep is Grep module.
+See pm-grep
+
+=head1 AUTHOR
+
+toritori0318 E<lt>toritoriopn@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+pm-grep
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
